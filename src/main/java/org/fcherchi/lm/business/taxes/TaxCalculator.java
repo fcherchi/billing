832b71efe191c3581da2e7c6e861bf36aea7cff9
Fernando <fcherchi@hotmail.com>
2021-08-19 10:53:33 +0200

    Tax Calculator added with rounding up logic

--- src/main/java/org/fcherchi/lm/business/taxes/TaxCalculator.java
+++ src/main/java/org/fcherchi/lm/business/taxes/TaxCalculator.java
@@ -3,45 +3,105 @@ package org.fcherchi.lm.business.taxes;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+/**
+ * Calculates the taxes according to the price given by configuration
+ */
 public class TaxCalculator {
 
-    public static final int PRICE_DECIMAL_PRECISION = 2;
     public static final int SALES_TAX_ROUND_UP_PRECISION = 2;
+    private static final double ROUNDUP_FACTOR = 0.05;
+
     private final TaxConfiguration taxConfiguration;
 
     public TaxCalculator(TaxConfiguration taxConfiguration) {
         this.taxConfiguration = taxConfiguration;
     }
 
+    /**
+     * Gets the price adding the sales tax accoring to tax configuration
+     * @param price The price without taxes
+     * @return The price plus sales taxes
+     */
     public double getPricePlusSalesTax(double price) {
-        return truncate(price + getSalesTaxFor(price));
+        BigDecimal bd = new BigDecimal(Double.toString(price));
+        BigDecimal result = bd.add(new BigDecimal(Double.toString(getSalesTaxFor(price))));
+
+        return result.doubleValue();
     }
 
-    public double getImportTaxFor(double price) {
-        return getTax(price, this.taxConfiguration.getImportTax());
-    }
-
+    /**
+     * Gets the price adding the import tax to the given raw price.
+     * @param rawPrice The price without taxes
+     * @return
+     */
     public double getPricePlusImportTax(double rawPrice) {
-        return truncate(rawPrice + getImportTaxFor(rawPrice));
+        return rawPrice + getImportTaxFor(rawPrice);
     }
 
+    /**
+     * Gets the final price adding both import taxes and sales taxes
+     * @param rawPrice The price without taxes
+     * @return The price plus sales and import taxes
+     */
+    public double getPricePlusSalesAndImportTax(double rawPrice) {
+        return rawPrice + getImportTaxFor(rawPrice) + getSalesTaxFor(rawPrice);
+    }
+
+
+    /**
+     * Gets the sales tax of the given price.
+     * @param price The price without taxes
+     * @return The Sales tax part to be added to the received price
+     */
     public double getSalesTaxFor(double price) {
         return getTax(price, this.taxConfiguration.getSalesTax());
     }
 
+    /**
+     * Gets the import tax of the given price.
+     * @param price The price without taxes
+     * @return The Import tax part to be added to the received price
+     */
+    public double getImportTaxFor(double price) {
+        return getTax(price, this.taxConfiguration.getImportTax());
+    }
+
+
+    /**
+     * Gets the applicable tax rounded up to the nearest 0.05
+     * @param value  The price to be taxed
+     * @param taxRate The tax rate
+     * @return The tax part of the price
+     */
     private double getTax(double value, double taxRate) {
         return round(value*taxRate/100);
     }
 
+    /**
+     * Rounds up to the nearest 0.05 (i.e. 2.31 -> 2.35)
+     * @param value the value to round up
+     * @return the rounded up value.
+     */
     private double round(double value) {
-        BigDecimal bd = new BigDecimal(value);
-        bd = bd.setScale(SALES_TAX_ROUND_UP_PRECISION, RoundingMode.HALF_UP);
-        return bd.doubleValue();
+        BigDecimal bd = new BigDecimal(Double.toString(value));
+        //this line truncates the value to 2 decimals
+        bd = bd.setScale(SALES_TAX_ROUND_UP_PRECISION, RoundingMode.FLOOR);
+
+        //only round up factor of the form 0.0x are allowed. In the example the divisible factor is 5
+        int divisibleFactor = (int) (ROUNDUP_FACTOR * 100);
+
+        //we get the power of 10 we have to use as multiplier in order to remove the decimal separator
+        long multiplier = (long) Math.pow(10, SALES_TAX_ROUND_UP_PRECISION);
+        long valueWithoutDecimals = (long) (bd.doubleValue() * multiplier);
+
+        //we get the modulus by divide to the divisible factor. i.e. from 1352 we get 2
+        int mod = (int) (valueWithoutDecimals % divisibleFactor);
+        // if not a round number (multiple of 5)
+        if (mod != 0) {
+            // go to the next multiple of 5 (from 1352 we do 1352 + (5 - 2)
+            valueWithoutDecimals = (int) (valueWithoutDecimals + divisibleFactor - mod);
+        }
+        return (double) valueWithoutDecimals / multiplier;
     }
 
-    private double truncate(double value) {
-        BigDecimal bd = new BigDecimal(value);
-        bd = bd.setScale(PRICE_DECIMAL_PRECISION, RoundingMode.DOWN);
-        return bd.doubleValue();
-    }
 }
--- src/test/java/org/fcherchi/lm/business/taxes/TaxCalculatorTest.java
+++ src/test/java/org/fcherchi/lm/business/taxes/TaxCalculatorTest.java
@@ -16,21 +16,27 @@ class TaxCalculatorTest {
     private TaxConfiguration taxConfiguration;
 
     @Test
-    void calculateSalesTax() {
+    void calculateOnlySalesTax() {
         TaxCalculator taxCalculator = new TaxCalculator(taxConfiguration);
         Mockito.when(taxConfiguration.getSalesTax()).thenReturn(10.0);
-        double cdPrice = 14.99;
-        double finalPrice = taxCalculator.getPricePlusSalesTax(cdPrice);
-        Assertions.assertEquals(16.49, finalPrice, "Expected 16.49 as per exercise example.");
+        Assertions.assertEquals(16.49, taxCalculator.getPricePlusSalesTax(14.99), "Wrong result as per exercise example.");
+        Assertions.assertEquals(20.89, taxCalculator.getPricePlusSalesTax(18.99), "Wrong result as per exercise example.");
     }
 
     @Test
     void calculateOnlyImportTax() {
         TaxCalculator taxCalculator = new TaxCalculator(taxConfiguration);
         Mockito.when(taxConfiguration.getImportTax()).thenReturn(5.0);
-        double rawPrice = 10.00;
-        double finalPrice = taxCalculator.getPricePlusImportTax(rawPrice);
-        Assertions.assertEquals(10.50, finalPrice, "Expected 10.50 as per exercise example.");
+        Assertions.assertEquals(10.50, taxCalculator.getPricePlusImportTax(10.00), "Wrong result as per exercise example.");
+        Assertions.assertEquals(11.85, taxCalculator.getPricePlusImportTax(11.25), "Wrong result as per exercise example.");
     }
 
+    @Test
+    void calculateImportAndSalesTax() {
+        TaxCalculator taxCalculator = new TaxCalculator(taxConfiguration);
+        Mockito.when(taxConfiguration.getImportTax()).thenReturn(5.0);
+        Mockito.when(taxConfiguration.getSalesTax()).thenReturn(10.0);
+        Assertions.assertEquals(54.65, taxCalculator.getPricePlusSalesAndImportTax(47.50), "EWrong result as per exercise example.");
+        Assertions.assertEquals(32.19, taxCalculator.getPricePlusSalesAndImportTax(27.99), "EWrong result as per exercise example.");
+    }
 }
